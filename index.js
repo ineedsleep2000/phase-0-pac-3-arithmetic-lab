39ed690e7eebc0f2360ba527c46d9447d289f715
Brian <turtle40965@gmail.com>
2024-02-27 13:34:44 -0800

    complete

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+function increment(n){
+    return ++n ;
+}
+
+function decrement(n){
+    return --n ;
+}
+
+// function makeInt(n){
+//     parseInt(n, 10);
+// }
+
+// function makeInt(n){
+//     parseInt(n, 1);
+// }
+
+function makeInt(n){
+   return parseInt(n, 10);
+}
+
+// function preserveDecimal(n{
+//     parseFloat(n);
+// })
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
